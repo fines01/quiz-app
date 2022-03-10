5dbfe4f87be7a3fc08825985f1071ff51e3a8961
I. Fritsch <ines.fritsch@gmail.com>
2022-03-10 17:43:49 +0100

    added questions

---
+++ script.js
@@ -0,0 +1,80 @@
+questions = [
+    {
+        "question": "Who invented HTML?",
+        "answer_1" : "",
+        "answer_2": "Tim Berners-Lee",
+        "answer_3": "",
+        "answer_4":"Bruce Lee",
+        "right_answer": 3,
+    },
+    {
+        "question": "What happens when JavaScript runs the alert() function?",
+        "answer_1": "JavaScript checks to see if there are any unprocessed events",
+        "answer_2": "A message is sent back to the PHP code to be logged on the server",
+        "answer_3": "JavaScript pops up a dialog box and execution continues until the </alert> tag is encountered",
+        "answer_4": "JavaScript execution is paused and a dialog box pops up",
+        "right_answer": 4,
+    },
+     {
+         "question": "Which of the following is not a way to include JavaScript in an HTML document?",
+         "answer_1": "By including the code the <?javascript and ?\> tags",
+         "answer_2": "By including the code between <script> and </script> tags",
+         "answer_3": "By including a file containing JavaScript using a tag",
+         "answer_4": "On a tag using an attribute like onclick=””",
+         "right_answer": 1,
+     },
+      {
+          "question": "What happens in a normal end-user’s browser when there is a JavaScript error?",
+          "answer_1": "JavaScript logs the error to the PHP error log",
+          "answer_2": "JavaScript prints a traceback indicating the line in error",
+          "answer_3": "JavaScript skips the line in error and continues executing after the next semicolon (;)",
+          "answer_4": "Nothing except perhaps a small red error icon that is barely noticeable",
+          "right_answer": 4,
+      },
+       {
+           "question": "Where can a developer find which line in a web page of JavaScript file is causing a syntax error?",
+           "answer_1": "By looking at a file on the hard disk of the system where the browser is running",
+           "answer_2": "In the developer console in the browser",
+           "answer_3": "Do a “View Source” to see the HTML source code",
+           "answer_4": "In the PHP error log",
+           "right_answer": 2,
+       },
+       {
+           "question": "Which of the following is not a valid JavaScript variable name?",
+           "answer_1": "3peat",
+           "answer_2": "$_data",
+           "answer_3": "$_DATA",
+           "answer_4": "_data",
+           "right_answer": 4,
+       },
+       {
+           "question": "What is the difference between strings with single quotes and double quotes in JavaScript?",
+           "answer_1": "Double quoted strings so variable substitution for variables that start with dollar sign ($)",
+           "answer_2": "There is no difference",
+           "answer_3": "Single quoted strings do not treat \n as a newline",
+           "answer_4": "Double quotes strings cannot be used in JavaScript",
+           "right_answer": 2,
+       },
+       {
+           "question": "What is the meaning of the “triple equals” operator (===) in JavaScript?",
+           "answer_1": "",
+           "answer_2": "",
+           "answer_3": "Both sides of the triple equals operator are converted to boolean before comparison",
+           "answer_4": "That the values being compared are the same without any type conversion",
+           "right_answer": 4,
+       },
+        {
+            "question": "How do you indicate that a variable reference within a JavaScript function is a global (i.e. not local) variable?",
+            "answer_1": "Use the keyword “global” to declare the variable in the function",
+            "answer_2": "Use the keyword “var” to declare the variable in the function",
+            "answer_3": "Nothing, simply declare the variable globally before the function definition in the code",
+            "answer_4": "Use the keyword “global” when declaring the variable outside the function",
+            "right_answer": 3,
+        },
+];
+
+
+
+function init(){
+
+}
