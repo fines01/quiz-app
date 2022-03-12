03a1e86fe3bffa81b82331f75f925a8c42fbe4e6
I. Fritsch <ines.fritsch@gmail.com>
2022-03-12 18:13:25 +0100

    refactoring

--- script.js
+++ script.js
@@ -1,80 +1,119 @@
-questions = [
-    {
-        "question": "Who invented HTML?",
-        "answer_1" : "",
-        "answer_2": "Tim Berners-Lee",
-        "answer_3": "",
-        "answer_4":"Bruce Lee",
-        "right_answer": 3,
-    },
-    {
-        "question": "What happens when JavaScript runs the alert() function?",
-        "answer_1": "JavaScript checks to see if there are any unprocessed events",
-        "answer_2": "A message is sent back to the PHP code to be logged on the server",
-        "answer_3": "JavaScript pops up a dialog box and execution continues until the </alert> tag is encountered",
-        "answer_4": "JavaScript execution is paused and a dialog box pops up",
-        "right_answer": 4,
-    },
-     {
-         "question": "Which of the following is not a way to include JavaScript in an HTML document?",
-         "answer_1": "By including the code the <?javascript and ?\> tags",
-         "answer_2": "By including the code between <script> and </script> tags",
-         "answer_3": "By including a file containing JavaScript using a tag",
-         "answer_4": "On a tag using an attribute like onclick=””",
-         "right_answer": 1,
-     },
-      {
-          "question": "What happens in a normal end-user’s browser when there is a JavaScript error?",
-          "answer_1": "JavaScript logs the error to the PHP error log",
-          "answer_2": "JavaScript prints a traceback indicating the line in error",
-          "answer_3": "JavaScript skips the line in error and continues executing after the next semicolon (;)",
-          "answer_4": "Nothing except perhaps a small red error icon that is barely noticeable",
-          "right_answer": 4,
-      },
-       {
-           "question": "Where can a developer find which line in a web page of JavaScript file is causing a syntax error?",
-           "answer_1": "By looking at a file on the hard disk of the system where the browser is running",
-           "answer_2": "In the developer console in the browser",
-           "answer_3": "Do a “View Source” to see the HTML source code",
-           "answer_4": "In the PHP error log",
-           "right_answer": 2,
-       },
-       {
-           "question": "Which of the following is not a valid JavaScript variable name?",
-           "answer_1": "3peat",
-           "answer_2": "$_data",
-           "answer_3": "$_DATA",
-           "answer_4": "_data",
-           "right_answer": 4,
-       },
-       {
-           "question": "What is the difference between strings with single quotes and double quotes in JavaScript?",
-           "answer_1": "Double quoted strings so variable substitution for variables that start with dollar sign ($)",
-           "answer_2": "There is no difference",
-           "answer_3": "Single quoted strings do not treat \n as a newline",
-           "answer_4": "Double quotes strings cannot be used in JavaScript",
-           "right_answer": 2,
-       },
-       {
-           "question": "What is the meaning of the “triple equals” operator (===) in JavaScript?",
-           "answer_1": "",
-           "answer_2": "",
-           "answer_3": "Both sides of the triple equals operator are converted to boolean before comparison",
-           "answer_4": "That the values being compared are the same without any type conversion",
-           "right_answer": 4,
-       },
-        {
-            "question": "How do you indicate that a variable reference within a JavaScript function is a global (i.e. not local) variable?",
-            "answer_1": "Use the keyword “global” to declare the variable in the function",
-            "answer_2": "Use the keyword “var” to declare the variable in the function",
-            "answer_3": "Nothing, simply declare the variable globally before the function definition in the code",
-            "answer_4": "Use the keyword “global” when declaring the variable outside the function",
-            "right_answer": 3,
-        },
-];
-
-
+let currentQuestion = 0; //startwert
+let rightAnswers = 0;
+let AUDIO_SUCCESS = new Audio('audio/success.mp3'); // od nur String mit Pfad hier speichern und eig Fkt. playAudio('path');
+let AUDIO_WRONG = new Audio('audio/wrong1.mp3');
 
 function init(){
 
+    displayQuestionCount();
+    showQuestion();
+
+    //toggleButtonDisable();
+
+}
+
+function displayQuestionCount() {
+    let questionCount = document.getElementsByClassName('amount-questions'); // returns html collection (array)
+    for (i = 0; i < questionCount.length; i++) {
+        // show amount of all questions
+        questionCount[i].innerHTML = questions.length;
+    }
+}
+
+function showQuestion() {
+    if ( gameOver() ) { //
+       showEndScreen();
+    } else {
+        updateProgressBar();
+        getNextQuestion();
+    }
+}
+
+function gameOver() {
+    return currentQuestion >= questions.length;
+}
+
+function correctAnswer(question, clickedAnswer) {
+    let clickedAnswerNumber = clickedAnswer.slice(-1); // cut out last character (index -1) of a string
+    return question["right_answer"] == clickedAnswerNumber;
+}
+
+function showEndScreen() {
+     document.getElementById('end-screen').style = ''; // auf html-attribut(!) "style" zugreifen (nicht direckt css style attribut in style.css datei?)
+     document.getElementById('question-body').style = 'display: none';
+     //document.getElementById('card-image').src = 'img/spacecat.jpg'; // attribut src ändern
+     document.getElementById('card-image').style = ''
+}
+
+function updateProgressBar() {
+    let progressBar = document.getElementById('progress-bar');
+    // calculate progress in percent and round result
+    let percent = ((currentQuestion + 1) / questions.length) * 100;
+    percent = Math.round(percent);
+    // update progress-bar
+    progressBar.innerHTML = `${percent} %`;
+    progressBar.style.width = `${percent}%` //auf css style attr zureifen vs html attribut?? // oder: .style = 'width: ${}%'
+}
+
+function getNextQuestion() {
+    // update to next question
+    let question = questions[currentQuestion];
+    document.getElementById('question-number').innerHTML = currentQuestion + 1;
+    document.getElementById('right-questions-amount').innerHTML = rightAnswers;
+    document.getElementById('question-title').innerHTML = question['question'];
+    for (let i = 1; i < 5; i++) {
+        document.getElementById(`answer-${i}`).innerHTML = question[`answer_${i}`];
+    }
+}
+
+//refactor function
+function displayAnswer(clickedAnswerId) {
+    let question = questions[currentQuestion];
+    let correctAnswerId = `answer-${question['right_answer']}`;
+    
+    //enable next-button
+    toggleButtonDisable();
+
+    if ( correctAnswer(question, clickedAnswerId) ) { 
+        document.getElementById(clickedAnswerId).parentNode.classList.add('bg-success');
+        rightAnswers++;
+        AUDIO_SUCCESS.play();
+    } else{ 
+        document.getElementById(clickedAnswerId).parentNode.classList.add('bg-danger');
+        document.getElementById(correctAnswerId).parentNode.classList.add('bg-success');
+        AUDIO_WRONG.play();
+    }
+}
+
+function nextQuestion() {
+    currentQuestion++;
+    //document.getElementById('next-btn').disabled = true;
+    toggleButtonDisable();
+    resetAnswerButtons();
+    showQuestion();
+}
+
+function resetAnswerButtons(){
+    for(let i=1; i<5; i++){
+        document.getElementById(`answer-${i}`).parentNode.classList.remove('bg-success');
+        document.getElementById(`answer-${i}`).parentNode.classList.remove('bg-danger');
+    }
+}
+
+function restartQuiz() {
+    rightAnswers = 0;
+    currentQuestion = 0;
+    document.getElementById('card-image').style = 'display:none'; // attribut src ändern
+    document.getElementById('end-screen').style = 'display:none'; // auf html-attribut(!) "style" zugreifen (nicht direckt css style attribut in style.css datei?)
+    document.getElementById('question-body').style = '';
+    showQuestion();
+}
+
+function toggleButtonDisable() {
+    let nextBtn = document.getElementById('next-btn');
+    if (nextBtn.disabled){
+        nextBtn.disabled = false;
+    } else {
+        nextBtn.disabled = true;
+    }
 }
